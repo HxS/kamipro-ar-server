a7e8809861aad9d2151adec05dd4b67bd7db0ab6
Genki Sakanashi <genki@e13.jp>
2016-05-04 15:24:00 +0900

    Add admin/admins

---
+++ app/controllers/admin/admins_controller.rb
@@ -0,0 +1,69 @@
+class Admin::AdminsController < ApplicationController
+  before_action :set_admin, only: [:show, :edit, :update, :destroy]
+
+  # GET /admins
+  def index
+    @admins = Admin.all
+  end
+
+  # GET /admins/1
+  def show
+  end
+
+  # GET /admins/new
+  def new
+    @admin = Admin.new
+  end
+
+  # GET /admins/1/edit
+  def edit
+  end
+
+  # POST /admins
+  def create
+    @admin = Admin.new(admin_params)
+
+    respond_to do |format|
+      if @admin.save
+        format.html { redirect_to [:admin, @admin], notice: 'Admin was successfully created.' }
+        #format.json { render :show, status: :created, location: @admin }
+      else
+        format.html { render :new }
+        #format.json { render json: @admin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /admins/1
+  def update
+    respond_to do |format|
+      if @admin.update(admin_params)
+        format.html { redirect_to [:admin, @admin], notice: 'Admin was successfully updated.' }
+        #format.json { render :show, status: :ok, location: @admin }
+      else
+        format.html { render :edit }
+        #format.json { render json: @admin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /admins/1
+  def destroy
+    @admin.destroy
+    respond_to do |format|
+      format.html { redirect_to admins_url, notice: 'Admin was successfully destroyed.' }
+      #format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_admin
+      @admin = Admin.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def admin_params
+			params.require(:admin).permit(:email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'admin/index'
+
+  namespace :admin do
+    resources :admins
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
