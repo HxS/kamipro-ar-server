acb62e3c44201d8dcdccf350d310be60a27bc171
Genki Sakanashi <e13h06@gmail.com>
2016-05-07 17:23:22 +0900

    Add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+	rescue_from Exception, with: :handle_500 unless Rails.env.development?
+
+	def handle_500(exception = nil)
+		render json: { message: '内部エラー' }, status: 500
+	end
+
 end
