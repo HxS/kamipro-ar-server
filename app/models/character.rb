e3e326c1f31b4e437f7abfce777ae5cb75a8349d
Genki Sakanashi <e13h06@gmail.com>
2016-06-20 00:58:25 +0900

    Implement character validations

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,7 @@ class Character < ActiveRecord::Base
   has_many :companies
 
   mount_uploader :thumbnail, CharacterThumbnailUploader
+
+  validates :name, presence: true
+  validates :thumbnail, presence: true
 end
