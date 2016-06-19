969fb611c46c0a6731c9543939f106a3658ce210
Genki Sakanashi <e13h06@gmail.com>
2016-06-20 00:51:38 +0900

    Implement staff validations

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,4 +1,8 @@
 class Staff < ActiveRecord::Base
-  has_secure_password
   belongs_to :company
+
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
