3686cc207ad28d563531d60ff54b2cc3221228aa
Genki Sakanashi <e13h06@gmail.com>
2016-06-20 00:56:19 +0900

    Add :company_id validation

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,6 +2,7 @@ class Staff < ActiveRecord::Base
   belongs_to :company
 
   validates :name, presence: true
+  validates :company_id, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   has_secure_password
