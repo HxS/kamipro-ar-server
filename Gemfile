d1fd3fd88ef5788bd56043105f227f7fadc7c870
Genki Sakanashi <e13h06@gmail.com>
2016-06-23 13:27:04 +0900

    Change to use cloudinary

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'rails_12factor', group: :production
 
 gem "version"
 
+gem 'cloudinary'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/uploaders/character_thumbnail_uploader.rb
+++ app/uploaders/character_thumbnail_uploader.rb
@@ -2,19 +2,26 @@
 
 class CharacterThumbnailUploader < CarrierWave::Uploader::Base
 
+  include Cloudinary::CarrierWave
+
+  process :tags => ['character_thumbnail']
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
   # storage :fog
+
+  version :thumbnail do
+      process resize_to_fit: [50, 50]
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  #def store_dir
+  #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
