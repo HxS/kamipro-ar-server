014ae73229eaafd20486980602872b6dc85448c8
Taisuke Miyazaki <imishinist@gmail.com>
2016-06-28 18:20:59 +0900

    remove rescue_from.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from Exception, with: :handle_500 unless Rails.env.development?
-
-  def handle_500(exception = nil)
-    render json: { message: '内部エラー' }, status: 500
-  end
-
   def authenticate_staff
     redirect_to sign_in_company_staffs_path unless session[:id]
   end
