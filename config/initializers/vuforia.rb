35ab07fbda9518f0cfc45bcf605638b12b575a4a
Taisuke Miyazaki <imishinist@gmail.com>
2016-07-13 17:57:54 +0900

    vuforiaのaccess_keyとsecret_keyを更新

--- config/initializers/vuforia.rb
+++ config/initializers/vuforia.rb
@@ -1,2 +1,4 @@
-Rails.application.config.vuforia_access_key = "76cee083a499314e8d537ebb69b5cb84ff36fe13"
-Rails.application.config.vuforia_secret_key = "d498e97c57e9ff9f0f3eca9a9b4e18ab5d8afeb5"
+# Rails.application.config.vuforia_access_key = "76cee083a499314e8d537ebb69b5cb84ff36fe13"
+# Rails.application.config.vuforia_secret_key = "d498e97c57e9ff9f0f3eca9a9b4e18ab5d8afeb5"
+Rails.application.config.vuforia_access_key = "7e815adb47e8f293e207b436292074046be11e9c"
+Rails.application.config.vuforia_secret_key = "e88ebbcf6e88afb5a7c9f475ee438c06e6eb2186"
